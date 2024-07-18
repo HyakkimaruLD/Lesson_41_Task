acddf7a2b8b56a3c39338dcaf437617d8e85c7de
Vlad Chernukha <forreadtr@gmail.com>
2024-07-18 19:51:37 +0300

    add Dealership

---
+++ Lesson_41_Task/Dealership.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <string>
+#include <vector>
+using namespace std;
+
+
+class Dealership {
+
+private:
+    string name;
+    vector<Car> cars;
+    vector<Application> applications;
+
+public:
+    Dealership();
+    Dealership(const string& name);
+
+    string getName() const;
+    void setName(const string& name);
+
+
+    void addCar(const Car& car);
+    void removeCar(const string& model);
+    Car* findCar(const string& model);
+
+
+    void addApplication(const Application& application);
+    void removeApplication(const string& applicantName);
+    Application* findApplication(const string& applicantName);
+    void editApplication(const string& applicantName, const string& newStatus);
+
+
+};
