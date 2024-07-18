554d855c3a6928e2d3e6cf4653ddd390cc91f477
Yaroslav <59771733+keyoneinc@users.noreply.github.com>
2024-07-18 20:03:50 +0100

    added some code to main, not finished

---
+++ Lesson_41_Task/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include "Application.h"
+#include "Car.h"
+#include "Dealership.h"
+#include "DelayedPurchaseApplication.h"
+#include "StandPurchaseApplication.h"
+using namespace std;
+
+int main()
+{
+	Dealership dealership("Mercedes Dealer");
+
+	Car aClass("Mercedes-Benz A-Class", 5, 35000.0, 15, true);
+	Car eClass("Mercedes-Benz E-Class", 5, 54000.0, 8, true);
+	Car sClass("Mercedes-Benz S-Class", 5, 94000.0, 5, true);
+
+	dealership.addCar(aClass);
+	dealership.addCar(eClass);
+	dealership.addCar(sClass);
+
+
+
+
+}
