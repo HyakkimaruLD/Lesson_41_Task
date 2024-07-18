94e25c14240a135b74d23b8626d244d72b8174fb
Yaroslav <59771733+keyoneinc@users.noreply.github.com>
2024-07-18 19:34:29 +0100

    created classes, deleted empty lines created standpurchaseapplication class, and added its definition; created delayedpurchaseapplication class

---
+++ Lesson_41_Task/DelayedPurchaseApplication.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Application.h"
+
+class DelayedPurchaseApplication
+{
+private:
+	double discountPrecentage;
+public:
+	DelayedPurchaseApplication(const customerName, const phoneNumber, double discountPrecentage)
+		: Application(customerName, phoneNumber), discountPrecentage(discountPrecentage) {};
+
+};
+
